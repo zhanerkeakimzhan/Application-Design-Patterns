4aa1a3fe6f3eff2abe43a576aa400a9b76c02606
zhanerkeakimzhan <74965537+Zhanerke2506@users.noreply.github.com>
2022-11-19 15:15:52 +0600

    'empty'

---
+++ src/userRegistration.java
@@ -0,0 +1,12 @@
+public class userRegistration {
+    private int idUser;
+    private String firstName;
+    private String lastName;
+    private String gender;
+    private String email;
+    private String login;
+    private int idInstitute;
+    private int idDepartment;
+    private int idJob;
+
+}
