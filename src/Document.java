dae50865f4ebf2544b0d6e6d75d37547600305fc
AliyaNurmakhanova <aliyajeon91@gmail.com>
2022-11-20 20:12:21 +0600

    'updates'

---
+++ src/Document.java
@@ -0,0 +1,5 @@
+public abstract class Document {
+    IProtocol protocol = createProtocol();
+
+    public abstract IProtocol createProtocol();
+}
---
+++ src/Protocol_2.java
@@ -0,0 +1,7 @@
+public class Protocol_2 implements IProtocol{
+
+    @Override
+    public String protocolType(String type) {
+        return "Протокол 2";
+    }
+}
